80347fae408d2febc07ce198c3b46473e8faaaf8
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2023-03-30 23:21:52 +0500

    making home screen

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,18 +1,242 @@
-import Button from 'components/Button';
-import InputBar from 'components/InputBar';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StatusBar,
+  ImageBackground,
+  Image,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 import {useStyle} from './styles';
+import Background from '../../assets/Background.png';
+const {width, height} = Dimensions.get('window');
 
 const Home: React.FC = () => {
   const styles = useStyle();
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{'Hello! Imm Home screen'}</Text>
-      <InputBar />
-      <Button />
-    </View>
+    // <ScrollView
+    //   scrollEnabled={width < height}
+    //   contentContainerStyle={{flexGrow: 1, backgroundColor: 'red'}}>
+    <ImageBackground
+      style={styles.container}
+      source={Background}
+      resizeMode="cover">
+      <StatusBar hidden={true} />
+
+      <View style={styles.topContainer}>
+        <Image
+          style={styles.dp}
+          resizeMode="cover"
+          source={{uri: 'https://i.dawn.com/large/2022/03/622af3dc3fe4f.png'}}
+        />
+        <Text style={styles.name}>Sana Javed</Text>
+        <Text style={styles.profile}>Landlord</Text>
+        <View style={styles.subContainer}>
+          <View>
+            <Text style={styles.topTxt}>AED</Text>
+            <Text style={styles.midTxt}>5000</Text>
+            <Text style={styles.endTxt}>Commision</Text>
+          </View>
+          <View style={styles.centerContainer}>
+            <Text style={styles.topTxt}>AED</Text>
+            <Text style={styles.midTxt}>5000</Text>
+            <Text style={styles.endTxt}>Commision</Text>
+          </View>
+          <View>
+            <Text style={styles.topTxt}>AED</Text>
+            <Text style={styles.midTxt}>5000</Text>
+            <Text style={styles.endTxt}>Commision</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <ScrollView style={{flex: 1}}>
+          <Text style={styles.heading}>Dashboard</Text>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+          <View style={styles.itemContainer}>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+            <View style={styles.item}>
+              <Image source={require('../../assets/icons/Vector.png')} />
+              <Text style={styles.itemTxt}>My Bookings</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </ImageBackground>
+    // </ScrollView>
   );
 };
 
--- app/screens/Home/styles.ts
+++ app/screens/Home/styles.ts
@@ -12,13 +12,90 @@ export const useStyle = () => {
     StyleSheet.create({
       container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+
         backgroundColor: isDark ? 'black' : 'white',
       },
-      title: {
-        fontWeight: 'bold',
-        color: colors.primary,
+      topContainer: {
+        flex: 0.4,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      dp: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: 'blue',
+      },
+      name: {
+        fontSize: 24,
+        fontWeight: '500',
+        color: 'white',
+      },
+      profile: {
+        fontSize: 16,
+        fontWeight: '400',
+        color: 'white',
+      },
+      subContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '60%',
+        justifyContent: 'space-between',
+        marginTop: 15,
+      },
+      topTxt: {
+        color: 'white',
+        fontSize: 8,
+        fontWeight: '400',
+      },
+      midTxt: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: '400',
+        textAlign: 'center',
+      },
+      endTxt: {
+        color: 'white',
+        fontSize: 10,
+        fontWeight: '400',
+        textAlign: 'center',
+      },
+      centerContainer: {
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: 'white',
+        paddingHorizontal: 20,
+        borderStyle: 'dashed',
+      },
+      bottomContainer: {
+        flex: 0.6,
+        backgroundColor: 'white',
+        borderRadius: 25,
+        paddingVertical: 25,
+        paddingHorizontal: 35,
+      },
+      heading: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: 'black',
+      },
+      itemContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      item: {
+        minWidth: 75,
+        minHeight: 75,
+        backgroundColor: '#0349B1',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 9,
+      },
+      itemTxt: {
+        color: 'white',
+        fontSize: 8,
       },
     });
   return React.useMemo(() => styles(), [isDark]);
